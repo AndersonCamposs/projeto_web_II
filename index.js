9e606bebf3a51394feb67a2fe9935fac3d1d5bda
AndersonCamposs <anderson.camposs87@outlook.com>
2025-04-08 19:15:21 -0300

    Configurando o express  para recebimento de dados

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Aluno = require('./Aluno');
 
 const app = express();
-
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static('public')); 
