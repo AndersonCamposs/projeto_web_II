1e5ecf2830edda433b961d841bded2a021f10199
AndersonCamposs <anderson.camposs87@outlook.com>
2025-04-23 20:31:38 -0300

    Update Passageiro.js

--- models/Passageiro.js
+++ models/Passageiro.js
@@ -2,15 +2,20 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const Passageiro = Schema({
-    nome: { type: String, required: true },
-    cpf: { type: String, required: true },
-    rg: { type: String, required: false },
-    dataNascimento: { type: Date, required: true },
-    telefone: { type: String, required: false },
-    estado: { type: String, required: true },
-    cidade: { type: String, required: true },
-    logradouro: { type: String, required: true },
-    bairro: { type: String, required: true },
-    numeroResidencia: { type: Number, required: true },
-});
+const passageiroSchema = Schema(
+    {
+        nome: { type: String, required: true },
+        cpf: { type: String, required: true },
+        rg: { type: String, required: false },
+        dataNascimento: { type: Date, required: true },
+        telefone: { type: String, required: false },
+        estado: { type: String, required: true },
+        cidade: { type: String, required: true },
+        logradouro: { type: String, required: true },
+        bairro: { type: String, required: true },
+        numeroResidencia: { type: Number, required: true },
+    },
+    { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
+
+module.exports = mongoose.model("Passageiro", passageiroSchema);
