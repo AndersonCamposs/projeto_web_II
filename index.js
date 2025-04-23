55b1f5c5b6ce2cdf6adad9f68955dd85ec5cfd80
Anderson Campos <anderson.camposs87@outlook.com>
2025-04-23 09:10:24 -0300

    Conectando ao banco de dados

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
-const express = require('express');
-const Aluno = require('./Aluno');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static('public')); 
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static("public"));
 
+mongoose.connect(
+    "mongodb+srv://admin:acs1_admin@cluster0.0sacf0g.mongodb.net/projeto_web_II?retryWrites=true&w=majority&appName=Cluster0"
+);
 
+const Aluno = require("./models/Aluno");
 
-const lista = [];
-const aluno1 = new Aluno('LCA1', 'Joao', 'Licenciatura em Computação');
-const aluno2 = new Aluno('LCA2', 'Maria', 'Licenciatura em Computação');
-const aluno3 = new Aluno('LCA3', 'Pedro', 'Licenciatura em Computação');
+app.get("/alunos", async (req, res) => {
+    const status = req.query.s;
+    const lista = await Aluno.find();
+    res.render("aluno/relatorio", { lista, status });
+});
 
-lista.push(aluno1, aluno2, aluno3);
+app.get("/alunos/cadastrar", (req, res) => {
+    res.render("aluno/cadastrar");
+});
 
+app.get("/alunos/:matricula", async (req, res) => {
+    const matricula = Number(req.params.matricula);
+    const aluno = await Aluno.findOne({ matricula });
+    res.render("aluno/detalhe", { aluno });
+});
 
-app.get('/alunos', (req, res) => {  
-    const status = req.query.s; 
-    res.render('aluno/relatorio', {lista, status})
-})
+app.post("/alunos", async (req, res) => {
+    const { matricula, nome, curso } = req.body;
+    const novoAluno = new Aluno({
+        matricula,
+        nome,
+        curso,
+    });
+    await novoAluno.save();
+    res.redirect("/alunos?s=1");
+});
 
-app.get('/alunos/cadastrar', (req, res) => {
-    res.render('aluno/cadastrar')
-})
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
+// conteúdos pertinentes ao projeto
+app.get("/passageiros/cadastrar", (req, res) => {
+    res.render("passageiro/cadastrar");
+});
 
-app.get('/alunos/:matricula', (req, res) => {    
-    const matricula = req.params.matricula;
-    const aluno = lista.find((aluno) => aluno.matricula === matricula)
-    res.render('aluno/detalhe', {aluno})
-})
-
-app.post('/alunos', (req, res) => {
-    const {matricula, nome, curso} = req.body;
-    lista.push(new Aluno(matricula, nome, curso));
-    res.redirect('/alunos?s=1')
-})
-
-app.get('/', (req, res) => {
-    res.render('index', {aluno1});
-})
- 
 app.use((req, res) => {
-    res.status(404).render('404');
-})
+    res.status(404).render("404");
+});
 
-app.listen('5500', () => {
-    console.log("Server already be running at port 5500:\nhttp://localhost:5500/")
-})
+app.listen("5500", () => {
+    console.log(
+        "Server already be running at port 5500:\nhttp://localhost:5500/"
+    );
+});
