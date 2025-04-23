a4cbe5e4110556a81279d1ab64f19365aa20162d
Anderson Campos <anderson.camposs87@outlook.com>
2025-04-23 13:30:08 -0300

    Formulário de inclusão de passageiro

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config()
+require("dotenv").config();
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -51,8 +51,8 @@ app.get("/passageiros/cadastrar", (req, res) => {
 });
 
 app.post("/passageiros", async (req, res) => {
-    
-})
+    console.log(req.body);
+});
 
 app.use((req, res) => {
     res.status(404).render("404");
--- models/Passageiro.js
+++ models/Passageiro.js
@@ -4,11 +4,12 @@ const Schema = mongoose.Schema;
 
 const Passageiro = Schema({
     nome: { type: String, required: true },
-    dataNascimento: { type: Date, required: true },
     cpf: { type: String, required: true },
     rg: { type: String, required: false },
-    numeroTelefone: { type: String, required: false },
+    dataNascimento: { type: Date, required: true },
+    telefone: { type: String, required: false },
     estado: { type: String, required: true },
+    cidade: { type: String, required: true },
     logradouro: { type: String, required: true },
     bairro: { type: String, required: true },
     numeroResidencia: { type: Number, required: true },
