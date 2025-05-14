90d7193fd6085b6b838c9579c23102013ef61171
AndersonCamposs <158058420+AndersonCamposs@users.noreply.github.com>
2025-05-13 21:41:51 -0300

    Rotas de deletar e atualizar passageiro

--- routes/passageiroRoutes.js
+++ routes/passageiroRoutes.js
@@ -4,8 +4,9 @@ const passageiroController = require("../controllers/passageiroController");
 const routes = express.Router();
 
 routes.get("/", passageiroController.relatorio);
-routes.get("/cadastrar", passageiroController.cadastrar);
+routes.get("/cadastrar/:_id?", passageiroController.cadastrar);
 routes.get("/search/:cpf", passageiroController.buscarPorCpf);
+routes.get("/delete/:_id", passageiroController.deletar);
 routes.get("/:_id", passageiroController.detalhar);
 routes.post("/", passageiroController.salvar);
 
