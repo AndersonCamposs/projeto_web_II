2a24f53707069a0ca8ea9a6828cadd03bd0ee615
Anderson Campos <anderson.camposs87@outlook.com>
2025-05-14 22:43:49 -0300

    Rota de deletar

--- routes/vooRoutes.js
+++ routes/vooRoutes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 
 routes.get('/', vooControler.relatorio);
 routes.get('/cadastrar', vooControler.cadastrar);
+routes.get('/delete/:_id', vooControler.deletar);
 routes.get('/:_id', vooControler.detalhar);
 routes.post('/', vooControler.salvar);
 
