2a9b3079bdeb48f89205f21a7f49fc3f6439c7a0
Anderson Campos <anderson.camposs87@outlook.com>
2025-05-15 23:28:34 -0300

    Update formatterUtils.js

--- utils/formatterUtils.js
+++ utils/formatterUtils.js
@@ -10,4 +10,15 @@ function formatHour(date) {
   return `${d.getUTCHours().toString().padStart(2, '0')}:${d.getUTCMinutes().toString().padStart(2, '0')}`;
 }
 
-module.exports = { formatDate, formatHour };
+/* retorna a data e hora de um objeto date no formato "dd/MM/yyyy, hh:mm"*/
+function formatarDataHora(d) {
+  const date = new Date(d);
+  return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1)
+    .toString()
+    .padStart(
+      2,
+      '0',
+    )}/${date.getFullYear().toString()}, ${date.getUTCHours().toString().padStart(2, '0')}:${date.getUTCMinutes().toString().padStart(2, '0')}`;
+}
+
+module.exports = { formatDate, formatHour, formatarDataHora };
