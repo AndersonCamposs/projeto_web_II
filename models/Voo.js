fbd8cd7be812ec616aca65a5acf3563ff5d7b77f
AndersonCamposs <158058420+AndersonCamposs@users.noreply.github.com>
2025-06-03 19:45:29 -0300

    Adicionando campos de timestamp nos voos

--- models/Voo.js
+++ models/Voo.js
@@ -2,20 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const vooSchema = Schema({
-  cod: { type: String, required: true },
-  paisOrigem: { type: String, required: true },
-  estadoOrigem: { type: String, required: true },
-  cidadeOrigem: { type: String, required: true },
-  paisDestino: { type: String, required: true },
-  estadoDestino: { type: String, required: true },
-  cidadeDestino: { type: String, required: true },
-  data: { type: Date, required: true },
-  tipoVoo: {
-    type: String,
-    enum: ['Nacional', 'Internacional'],
-    default: 'Nacional',
+const vooSchema = Schema(
+  {
+    cod: { type: String, required: true },
+    paisOrigem: { type: String, required: true },
+    estadoOrigem: { type: String, required: true },
+    cidadeOrigem: { type: String, required: true },
+    paisDestino: { type: String, required: true },
+    estadoDestino: { type: String, required: true },
+    cidadeDestino: { type: String, required: true },
+    data: { type: Date, required: true },
+    tipoVoo: {
+      type: String,
+      enum: ['Nacional', 'Internacional'],
+      default: 'Nacional',
+    },
   },
-});
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
+);
 
 module.exports = mongoose.model('Voo', vooSchema);
