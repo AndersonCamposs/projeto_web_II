d6dde3a10c3602c7c19542c974313a7b73b04728
Anderson Campos <anderson.camposs87@outlook.com>
2025-06-15 15:57:08 -0300

    Adicionando formatador de número de telefone

--- public/js/formatterUtils.js
+++ public/js/formatterUtils.js
@@ -7,3 +7,19 @@ document.querySelector('#cpfInput').addEventListener('input', (e) => {
 
   e.target.value = cpf;
 });
+
+document.querySelector('#telefoneInput').addEventListener('input', (e) => {
+  let telefone = e.target.value.replace(/[^0-9]/g, '');
+
+  if (telefone.length > 2) {
+    telefone = `(${telefone.slice(0, 2)}) ${telefone.slice(2)}`;
+  }
+  if (telefone.length > 10) {
+    telefone = `(${telefone.slice(1, 3)}) ${telefone.slice(5, 10)}-${telefone.slice(10)}`;
+  }
+
+  e.target.value = telefone;
+
+  // (xx) xxxxx-xxxx
+  // xxxxxxx
+});
